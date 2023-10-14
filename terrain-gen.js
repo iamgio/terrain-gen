311814dd96d030c3928038abd43a3c626d98db0b
Giorgio Garofalo <giogar2705@gmail.com>
2023-10-14 20:12:31 +0200

    Add random generation

--- terrain-gen.js
+++ terrain-gen.js
@@ -1,32 +1,43 @@
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 500;
 
-const TRANSLATION_Y = -300;
-const TRANSLATION_Z = -700;
+const TRANSLATION_Y = -700;
+const TRANSLATION_Z = -1400;
 
-const ROWS = 50;
-const COLS = 30;
+const ROWS = 40;
+const COLS = 40;
 const TILE_SIZE = 40;
+
+const RANDOM_MAX = 50;
+
+let heights = [];
 
 function setup() {
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, WEBGL);
+
+    for (let x = 0; x < COLS; x++) {
+        heights[x] = [];
+        for (let y = 0; y < ROWS; y++) {
+            heights[x][y] = Math.random() * RANDOM_MAX;
+        }
+    }
 }
 
 function draw() {
     background(0);
     noFill();
     stroke(255);
-    strokeWeight(0.5);
+    strokeWeight(0.7);
 
     translate(-COLS * TILE_SIZE / 2, TRANSLATION_Y, TRANSLATION_Z);
 
-    rotateX(1);
+    rotateX(0.9);
 
-    for (let y = 0; y < ROWS; y++) {
+    for (let y = 0; y < ROWS - 1; y++) {
         beginShape(TRIANGLE_STRIP);
         for (let x = 0; x < COLS; x++) {
-            vertex(x * TILE_SIZE, y * TILE_SIZE);
-            vertex(x * TILE_SIZE, (y + 1) * TILE_SIZE);
+            vertex(x * TILE_SIZE, y * TILE_SIZE, heights[x][y]);
+            vertex(x * TILE_SIZE, (y + 1) * TILE_SIZE, heights[x][y + 1]);
         }
         endShape();
     }
